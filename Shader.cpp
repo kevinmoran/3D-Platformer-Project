b8d3adfc5cf7f49c085d76931fc015128cf0370d
Kevin <kevinmoran007@gmail.com>
2018-02-26 12:42:03 +0000

    Shader code cleanup Removed unneeded sprintf and strcat Reduced duplicated shader loading code into function Read entire shader file at once instead of calling fgets for every line General tidy up

--- Shader.cpp
+++ Shader.cpp
@@ -1,157 +1,43 @@
 #include "Shader.h"
 
 #include <stdio.h>
-#include <string.h> //strcat
+#include <string.h> //strlen
 
-#include "gl_lite.h"
+#include "utils.h"
 
-#define SHADER_PATH "Shaders/"
+#define SHADERS_FOLDER "Shaders/"
 
-static bool load_shader_program(const char* vert_file, const char* frag_file, GLuint* id);
+//Internal functions
+static bool _load_shader_program(Shader* shader, const char* vert_file, const char* frag_file);
+static bool _load_and_compile_shader(GLuint* handle, const char* filename, GLuint shader_type);
+static void _concat_strings(const char *string_a, size_t a_length, const char *string_b, size_t b_length, char *dest, size_t dest_length);
 
-Shader init_shader(const char* vert_file, const char* frag_file){
-    Shader temp;
-    if(!load_shader_program(vert_file, frag_file, &temp.id)){
+Shader init_shader(const char* vert_file, const char* frag_file)
+{
+    Shader result = {};
+    result.vert_file = vert_file;
+    result.frag_file = frag_file;
+
+    if(!_load_shader_program(&result, vert_file, frag_file)){
         //handle failure? default shader program?
-        temp.compiled = false;
-        return temp;
+        return result;
     }
-    temp.compiled = true;
-    temp.M_loc = glGetUniformLocation(temp.id, "M");
-    temp.V_loc = glGetUniformLocation(temp.id, "V");
-    temp.P_loc = glGetUniformLocation(temp.id, "P");
-    return temp;
+    result.compiled = true;
+    result.M_loc = glGetUniformLocation(result.id, "M");
+    result.V_loc = glGetUniformLocation(result.id, "V");
+    result.P_loc = glGetUniformLocation(result.id, "P");
+    return result;
 }
 
-static bool load_shader_program(const char* vert_file, const char* frag_file, GLuint* id){
-
-    GLuint vs, fs;
-    //Load vertex shader
-    char vs_string[65536];
-    {
-        vs_string[0] = '\0';
-        char vert_file_path[64];
-        sprintf(vert_file_path, "%s%s", SHADER_PATH, vert_file);
-        
-        FILE* fp = fopen(vert_file_path, "r");
-        if(!fp){
-            fprintf(stderr, "ERROR opening vertex shader file: %s\n", vert_file_path);
-            return false;
-        }
-        char line[1024];
-        while(fgets(line, 1024, fp)){
-            strcat(vs_string, line);
-        }
-        fclose(fp);
-    }
-    //Compile vertex shader
-    {
-        vs = glCreateShader(GL_VERTEX_SHADER);
-        const char* vs_pointer = vs_string;
-        glShaderSource(vs, 1, &vs_pointer, NULL);
-        glCompileShader(vs);
-        
-        //Check for compilation errors
-        GLint params = -1;
-        glGetShaderiv(vs, GL_COMPILE_STATUS, &params);
-        if(params!=GL_TRUE){
-            fprintf(stderr,"ERROR: vertex shader `%s` did not compile\n", vert_file);
-            int log_length = 0;
-            char shader_log[2048];
-            glGetShaderInfoLog(vs, 2048, &log_length, shader_log);
-            fprintf(stderr, "shader info log for GL index %u: \n%s\n", vs, shader_log);
-            glDetachShader(*id, vs);
-            glDeleteShader(vs);
-            return false;
-        }
-    }
-    
-    //Load fragment shader
-    char fs_string[65536];
-    {
-        fs_string[0] = '\0';
-        char frag_file_path[64];
-        sprintf(frag_file_path, "%s%s", SHADER_PATH, frag_file);
-        
-        FILE* fp = fopen(frag_file_path, "r");
-        if(!fp){
-            fprintf(stderr, "ERROR opening fragment shader file: %s\n", frag_file_path);
-            glDetachShader(*id, vs);
-            glDeleteShader(vs);
-            return false;
-        }
-        char line[1024];
-        while(fgets(line, 1024, fp)){
-            strcat(fs_string, line);
-        }
-        fclose(fp);
-    }
-    //Compile fragment shader
-    {
-        fs = glCreateShader(GL_FRAGMENT_SHADER);
-        const char* fs_pointer = fs_string;
-        glShaderSource(fs, 1, &fs_pointer, NULL);
-        glCompileShader(fs);
-        
-        //Check for compilation errors
-        GLint params = -1;
-        glGetShaderiv(fs, GL_COMPILE_STATUS, &params);
-        if(params!=GL_TRUE){
-            fprintf(stderr,"ERROR: fragment shader `%s` did not compile\n", frag_file);
-            int log_length = 0;
-            char shader_log[2048];
-            glGetShaderInfoLog(fs, 2048, &log_length, shader_log);
-            fprintf(stderr, "shader info log for GL index %u: \n%s\n", fs, shader_log);
-            glDetachShader(*id, vs);
-            glDetachShader(*id, fs);
-            glDeleteShader(vs);
-            glDeleteShader(fs);
-            return false;
-        }
-    }
-    
-    //Create and link program
-    *id = glCreateProgram();
-    glAttachShader(*id, vs);
-    glAttachShader(*id, fs);
-
-    glBindAttribLocation(*id, VP_ATTRIB_LOC, "vp");
-    glBindAttribLocation(*id, VT_ATTRIB_LOC, "vt");
-    glBindAttribLocation(*id, VN_ATTRIB_LOC, "vn");
-
-    glLinkProgram(*id);
-    
-    //Check for linking errors
-    {
-        GLint params = -1;
-        glGetProgramiv(*id, GL_LINK_STATUS, &params);
-        if(params != GL_TRUE){
-            fprintf(stderr, "ERROR linking shader program with GL index %u\n", *id);
-            int log_length = 0;
-            char prog_log[2048];
-            glGetProgramInfoLog(*id, 2048, &log_length, prog_log);
-            fprintf(stderr, "program info log for GL index %u:\n%s\n", *id, prog_log);
-            return false;
-        }
-    }
-
-    glDetachShader(*id, vs);
-    glDetachShader(*id, fs);
-    glDeleteShader(vs);
-    glDeleteShader(fs);
-    
-    return true;
-}
-
-bool reload_shader_program(const char* vert_file, const char* frag_file, Shader* s){
-    delete_program(s);
-    if(!load_shader_program(vert_file, frag_file, &(s->id))) {
-        fprintf(stderr, "ERROR in reload_shader_program using vert shader %s and frag shader %s", vert_file, frag_file);
+bool reload_shader_program(Shader* shader){
+    delete_program(shader);
+    if(!_load_shader_program(shader, shader->vert_file, shader->frag_file)) {
+        fprintf(stderr, "ERROR in reload_shader_program using vert shader %s and frag shader %s", shader->vert_file, shader->frag_file);
         return false;
     }
-    s->M_loc = glGetUniformLocation(s->id, "M");
-    s->V_loc = glGetUniformLocation(s->id, "V");
-    s->P_loc = glGetUniformLocation(s->id, "P");
+    shader->M_loc = glGetUniformLocation(shader->id, "M");
+    shader->V_loc = glGetUniformLocation(shader->id, "V");
+    shader->P_loc = glGetUniformLocation(shader->id, "P");
     return true;
 }
 
@@ -165,4 +51,110 @@ void delete_program(Shader* s){
         s->P_loc = -1;
         s->compiled = false;
     }
+}
+
+static bool _load_shader_program(Shader* shader, const char* vert_file, const char* frag_file)
+{
+    GLuint vs, fs;
+    if(!_load_and_compile_shader(&vs, vert_file, GL_VERTEX_SHADER))
+        return false;
+    if(!_load_and_compile_shader(&fs, frag_file, GL_FRAGMENT_SHADER))
+        return false;
+    
+    //Create and link program
+    shader->id = glCreateProgram();
+    glAttachShader(shader->id, vs);
+    glAttachShader(shader->id, fs);
+
+    glBindAttribLocation(shader->id, VP_ATTRIB_LOC, "vp");
+    glBindAttribLocation(shader->id, VT_ATTRIB_LOC, "vt");
+    glBindAttribLocation(shader->id, VN_ATTRIB_LOC, "vn");
+
+    glLinkProgram(shader->id);
+    
+    bool result = true;
+
+    //Check for linking errors
+    {
+        GLint params = -1;
+        glGetProgramiv(shader->id, GL_LINK_STATUS, &params);
+        if(params != GL_TRUE){
+            int log_length = 0;
+            char prog_log[2048];
+            glGetProgramInfoLog(shader->id, 2048, &log_length, prog_log);
+            fprintf(stderr, "ERROR linking shader program. Program info log:\n%s\n", prog_log);
+            result = false;
+        }
+    }
+    glDetachShader(shader->id, vs);
+    glDetachShader(shader->id, fs);
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+    
+    return result;
+}
+
+static bool _load_and_compile_shader(GLuint* handle, const char* filename, GLuint shader_type)
+{
+    assert((shader_type == GL_VERTEX_SHADER) || (shader_type == GL_FRAGMENT_SHADER));
+
+    //Read shader file into buffer
+    const int SHADER_BUFFER_LENGTH = 65536;
+    char shader_string[SHADER_BUFFER_LENGTH];
+    {
+        shader_string[0] = '\0';
+        const int FILENAME_BUFFER_LENGTH = 64;
+        char full_file_path[FILENAME_BUFFER_LENGTH];
+        _concat_strings(SHADERS_FOLDER, strlen(SHADERS_FOLDER), filename, strlen(filename), full_file_path, FILENAME_BUFFER_LENGTH);
+        
+        FILE *fp = fopen(full_file_path, "rb");
+        if(!fp){
+            fprintf(stderr, "ERROR opening shader file: %s\n", full_file_path);
+            return false;
+        }
+        
+        fseek(fp, 0, SEEK_END);
+        int file_length = ftell(fp);
+        assert(file_length < (SHADER_BUFFER_LENGTH - 1));
+        // char *shader_string = malloc(file_length + 1);
+
+        rewind(fp);
+        fread(shader_string, SHADER_BUFFER_LENGTH, 1, fp);
+        shader_string[file_length] = 0;
+
+        fclose(fp);
+    }
+    //Compile shader
+    {
+        *handle = glCreateShader(shader_type);
+        const char* temp = shader_string;
+        glShaderSource(*handle, 1, &temp, NULL);
+        glCompileShader(*handle);
+        
+        //Check for compilation errors
+        GLint params = -1;
+        glGetShaderiv(*handle, GL_COMPILE_STATUS, &params);
+        if(params != GL_TRUE){
+            int log_length = 0;
+            char shader_log[2048];
+            glGetShaderInfoLog(*handle, 2048, &log_length, shader_log);
+            fprintf(stderr, "ERROR: shader `%s` did not compile. Shader info log:\n%s\n", filename, shader_log);
+            glDeleteShader(*handle);
+            return false;
+        }
+    }
+    return true;
+}
+
+static void _concat_strings(const char *string_a, size_t a_length, const char *string_b, size_t b_length, char *dest, size_t dest_length)
+{
+    assert((a_length + b_length) < dest_length);
+
+    for(int i = 0; i < a_length; ++i){
+        *dest++ = *string_a++;
+    }
+    for(int i = 0; i < b_length; ++i){
+        *dest++ = *string_b++;
+    }
+    *dest++ = 0;
 }
--- Shader.h
+++ Shader.h
@@ -1,15 +1,19 @@
 #pragma once
 
+#include "gl_lite.h"
+
 #define VP_ATTRIB_LOC 0
 #define VT_ATTRIB_LOC 1
 #define VN_ATTRIB_LOC 2
 
 struct Shader {
     GLuint id;
+    const char* vert_file;
+    const char* frag_file;
     GLuint M_loc, V_loc, P_loc;
     bool compiled;
 };
 
 Shader init_shader(const char* vert_file, const char* frag_file);
-bool reload_shader_program(const char* vert_file, const char* frag_file, Shader* s);
+bool reload_shader_program(Shader* s);
 void delete_program(Shader* s);
